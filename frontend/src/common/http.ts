880e0e25e72c459317785cb06a2470cbb7cefd6f
adas77 <73692470+adas77@users.noreply.github.com>
2022-12-06 21:55:21 +0100

    Delete frontend directory

--- frontend/src/common/http.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    // TODO: baseURL: "http://localhost:${port}/${path}" to .env
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
--- frontend/src/types/user.type.ts
+++
@@ -1,7 +0,0 @@
-export default interface IUserData {
-    id?: any | null,
-    username: string,
-    email: string,
-    passwordHash: string,
-    // roles: string[],
-}
